05f85688116f73b1d5ef313d37cad0d83add39e1
Bryan Melanson <hello@bryan.horse>
2024-02-04 00:08:02 -0500

    Dual screens

--- .config/polybar/polybar.sh
+++ .config/polybar/polybar.sh
@@ -1,7 +1,3 @@
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload example &
-	done
-else
-	polybar --reload example &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload example &
+done
